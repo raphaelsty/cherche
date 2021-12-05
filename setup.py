c21c3efc3b8d51ffd20d8a5dac2b4bc70bb67ba5
raphaelsty <raphael.sourty@gmail.com>
2021-12-05 03:25:34 +0100

    update cherche

--- setup.py
+++ setup.py
@@ -18,11 +18,13 @@ setuptools.setup(
     install_requires=[
         "elasticsearch == 7.10.0",
         "flashtext == 2.7",
-        "scipy == 1.6.2",
-        "sentence-transformers==2.1.0",
-        "transformers == 4.12.0",
+        "numpy == 1.21.4",
         "pytest-cov == 3.0.0",
         "rank-bm25 == 0.2.1",
+        "scipy == 1.6.2",
+        "sentence-transformers==2.1.0",
+        "tqdm == 4.62.3",
+        "transformers == 4.12.0",
     ],
     package_data={},
     classifiers=[
